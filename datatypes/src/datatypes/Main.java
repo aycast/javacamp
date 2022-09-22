a1c5718a1eedd986aa815e2e37df5d0aecb2ea38
aycast <ayca-temel@hotmail.com>
2022-09-22 15:44:56 +0300

    java engin demiroğ lesson 4
    data types

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double number = 14.4;
+		number = -127;
+		char character = 'A';
+		boolean istrue =false;
+
+	}
+
+}
