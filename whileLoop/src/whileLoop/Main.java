7c63155a6f2430977e6f4f70ae340527992c4c6c
aycast <ayca-temel@hotmail.com>
2022-09-27 13:21:24 +0300

    java camp engin demiroğ 11
    while loop

---
+++ whileLoop/src/whileLoop/Main.java
@@ -0,0 +1,16 @@
+package whileLoop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i=2;
+		System.out.println("baslangic sayisi:"+i);
+		while(i<10) {
+			System.out.println("sayi:"+i);
+			i++;
+		}
+		System.out.println("while dongusu bitti");
+
+	}
+
+}
