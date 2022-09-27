654eb80567794b44cb3621f5de8bafc96f76d496
aycast <ayca-temel@hotmail.com>
2022-09-27 13:13:40 +0300

    java camp engin demiroğ 11
    for loop

---
+++ forLoop/src/forLoop/Main.java
@@ -0,0 +1,18 @@
+package forLoop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("1 ile 10 arasindaki tek sayilar : ");
+		for(int i=1;i<10;i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("1 ile 10 arasindaki cift sayilar:");
+		for(int j=2;j<10;j+=2) {
+			System.out.println(j);
+		}
+		System.out.println("Dongu bitti");
+
+	}
+
+}
