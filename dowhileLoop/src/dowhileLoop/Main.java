feb50e994dfa8555c04856faaadd4213a5bd6e44
aycast <ayca-temel@hotmail.com>
2022-09-27 13:34:43 +0300

    java camp engin demiroğ 13
    do while loop

---
+++ dowhileLoop/src/dowhileLoop/Main.java
@@ -0,0 +1,15 @@
+package dowhileLoop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int j=100;
+		do {
+			System.out.println(j);
+			j++;
+		}while(j<20);
+		System.out.println();
+		//do while in while dan farkı do while sart saglanmasa bile bir kez calisir
+	}
+
+}
